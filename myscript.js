da4ad482876443d0864c635ae092c098cef8f6ea
Andrii Stepaenko <andrii.stp@gmail.com>
2023-01-26 12:32:31 +0200

    39.4 repeta 1

---
+++ myscript.js
@@ -0,0 +1,23 @@
+const age = 10;
+const totalPrice = 200.74;
+const userName = "Chelsy";
+const message = "Welcome";
+const isOpen = true;
+const shouldConfirm = false;
+
+
+
+// console.log("price is:", totalPrice);
+
+
+const type = typeof isOpen;
+
+console.log(type);
+
+alert("hello");
+
+
+
+
+
+ 
