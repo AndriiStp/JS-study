c26dba26304d7814c6c8a3e7d59769330842c15d
Andrii Stepaenko <andrii.stp@gmail.com>
2023-01-27 09:51:22 +0200

    repete1/1

--- myscript.js
+++ myscript.js
@@ -1,23 +1,94 @@
-const age = 10;
-const totalPrice = 200.74;
-const userName = "Chelsy";
-const message = "Welcome";
-const isOpen = true;
-const shouldConfirm = false;
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = "Chelsy";
+// const message = "Welcome";
+// const isOpen = true;
+// const shouldConfirm = false;
 
 
 
 // console.log("price is:", totalPrice);
 
+// const type = typeof isOpen;
 
-const type = typeof isOpen;
+// console.log(type);
 
-console.log(type);
-
-alert("hello");
-
-
+// вікно без підтвердження. зупиняє виконання скрипта поки не виконається
+// alert("hello");
 
 
+// Віконце підтвердження
+// const shouldRenew = confirm("&&&&&&&&&");
+// console.log(shouldRenew);
 
- 
+// модалка з полем ля введення
+// let quantity = prompt("Input value");
+// quantity = Number(quantity); // привдим до числа
+// console.log(quantity);
+// console.log(typeof quantity);
+
+
+// Числа
+// let elementWidth = "50px";
+// elementWidth = Number.parseInt(elementWidth);
+// console.log("elementWidth: ", elementWidth);
+
+
+// float
+// let elementHeight = "200.56";
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log("elementHeight", elementHeight);
+
+
+// let salary = 1250.8234687346;
+// salary = Number(salary.toFixed(2));
+// console.log(salary);
+
+
+
+// const blscklidtedWord1 = 'spam';
+// const blscklidtedWord2 = 'sale';
+
+
+// const string1 = 'Hello, my name is Vasya, its not spam,  Ill give you 1 million';
+// const string2 = 'The largest SALE this week, dont miss';
+// const string3 = 'Marketing company #fatlivesmatter';
+
+// console.log(string1.includes(blscklidtedWord1));
+// console.log(string1.includes(blscklidtedWord2));
+
+// console.log(string2.includes(blscklidtedWord1));
+// const normalizedString2 = string2.toLowerCase();
+
+// console.log(string2.toLowerCase().includes(blscklidtedWord2));
+
+
+// console.log(string3.includes(blscklidtedWord1));
+// console.log(string3.includes(blscklidtedWord2));
+
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log('Can open chat?', canOpenChat);
+
+// const sub = 'pro';
+
+// const canAccessContent = sub === 'pro' || sub === 'vip';
+// console.log('Have access to content?', canAccessContent);
+
+
+// const balance = 1000;
+// let message;
+
+// if (balance > 0) {
+//     message = 'Positive';
+// }
+// else {
+//     message = 'negative';
+// }
+// console.log(message); balance>=0
+
+// const message >= 0 ? 'Positive' : 'Negative';
